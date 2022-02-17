2fc629cbf4eb143a33d1118e63d00c82942e7d80
Unix13 <vivasjesus80@gmail.com>
2022-02-17 18:11:40 +0100

    Crea una nueva clase que llamarás califica y, utilizando las funciones de refactorización, mueve el método "califica" a la nueva clase. Sube las modificaciones al repositorios de Github, siendo el comentario el enunciado de este ejercicio.

--- Notas_4/src/notas/Notas.java
+++ Notas_4/src/notas/Notas.java
@@ -19,7 +19,7 @@ public class Notas {
 		System.out.print("Introduzca una nota: ");
 		nota = reader.nextInt();
 
-		calificacion = califica(nota);
+		calificacion = notas.califica.califica(nota);
 
 		System.out.println(calificacion);
 		reader.close();
@@ -28,28 +28,18 @@ public class Notas {
 	/**
 	 * @param nota
 	 * @return
-	 * @deprecated Use {@link #califica(int)} instead
+	 * @deprecated Use {@link notas.califica#califica(int)} instead
 	 */
 	public static String cali(int nota) {
-		return califica(nota);
+		return notas.califica.califica(nota);
 	}
 
 	/**
 	 * @param num_nota
 	 * @return
+	 * @deprecated Use {@link califica#califica(int)} instead
 	 */
 	public static String califica(int num_nota) {
-		String calificacion;
-		if (num_nota >= 0 && num_nota < 5)
-			calificacion = "Suspenso";
-		else if (num_nota >= 5 && num_nota < 7)
-			calificacion = "Aprobado";
-		else if (num_nota >= 7 && num_nota < 9)
-			calificacion = "Notable";
-		else if (num_nota >= 9 && num_nota <= 10)
-			calificacion = "Sobresaliente";
-		else
-			calificacion = "La nota introducida no es correcta";
-		return calificacion;
+		return califica.califica(num_nota);
 	}
 }
